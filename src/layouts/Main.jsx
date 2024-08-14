3214a74f45aaf1a9c55a59a3544b18c7785f24d0
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-08-15 00:29:13 +0600

    basic setup

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+    return (
+        <div>
+            {/* nav */}
+            <h1>Main</h1>
+            {/* footer */}
+            
+        </div>
+    );
+};
+
+export default Main;
